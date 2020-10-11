de661f57997252b884f1db123475b2d600084a8c
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-11 23:50:04 +0000

    fade and slide

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $('#button-create-item').on('click',function(){
          html += '<button class="item-add">Add to cart</button>';
          html += '<button class="item-remove">Remove</button>';
          html += '<br>';
-         html += '<a href="#">More info</a>';
+         html += '<a class="more-info-link" href="#">More info</a>';
          html += '<div class="more-info">Lorem ipsum dolor sit amet</div>';
         html += '</div>';
     
@@ -22,7 +22,7 @@ $('#button-create-item').on('click',function(){
     $('#container').on('click','.more-info-link',function(event){
         event.preventDefault();
         
-      $(this).parent().find('.more-info').toggle();
+      $(this).parent().find('.more-info').slideToggle();
     });
     
     $('#container').on('click','.item-remove',function(){
