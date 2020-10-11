1f01a7336082736a777e3a72ae16c285b7391225
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-11 22:26:46 +0000

    creating items dynamically

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-
+$('#button-create-item').on('click',function(){
+    let name = $('#input-create-item').val();
+    $('#input-create-item').val('');
+    
+    let html= '';
+     html += '<div class="item">';
+         html += '<div class="name">' + name +'</div>';
+          html += '<img src="assets/house.jpg"></img>';
+        html += ' <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>';
+         html += '<div class="price">499</div>';
+         html += '<button>Add to cart</button>';
+             html += '<br>';
+         html += '<a href="#">More info</a>';
+         html += '<div class="more-info">Lorem ipsum dolor sit amet</div>';
+        html += '</div>';
+    
+    $('#container').prepend(html);
+});
 });
