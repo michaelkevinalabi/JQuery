f5715eba7721a6c864459ad7e75f24f78ea4c942
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-11 22:39:04 +0000

    removing elements

--- js/main.js
+++ js/main.js
@@ -9,12 +9,17 @@ $('#button-create-item').on('click',function(){
           html += '<img src="assets/house.jpg"></img>';
         html += ' <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>';
          html += '<div class="price">499</div>';
-         html += '<button>Add to cart</button>';
-             html += '<br>';
+         html += '<button class="item-add">Add to cart</button>';
+         html += '<button class="item-remove">Remove</button>';
+         html += '<br>';
          html += '<a href="#">More info</a>';
          html += '<div class="more-info">Lorem ipsum dolor sit amet</div>';
         html += '</div>';
     
     $('#container').prepend(html);
 });
+    
+    $('#container').on('click','.item-remove',function(){
+        $(this).parent().remove();
+    });
 });
