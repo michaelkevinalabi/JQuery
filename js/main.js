118d0f0cad292aa0f94f31cf5bae53568b635077
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-12 02:23:42 +0000

    checkbox form and form post

--- js/main.js
+++ js/main.js
@@ -14,58 +14,81 @@ function addItem(id, name, description, price, moreInfo) {
   html += '<div class="more-info">' + moreInfo + '</div>';
   html += '</div>';
 
-    $('#container').prepend(html);  
+  $('#container').prepend(html);
 }
-    
-    $(document).ready(function(){
-    $('#container').on('click','.more-info-link',function(event){
-        event.preventDefault();
-        
-       $(this).parent().find('.more-info').slideToggle('slow');
+
+$(document).ready(function(){
+  $('#container').on('click','.more-info-link', function(event){
+    event.preventDefault();
+
+    $(this).parent().find('.more-info').slideToggle('slow');
     $(this)
       .animate({ "opacity": 0.5, "margin-left": 10 }, 150)
       .animate({ "opacity": 1.0, "margin-left": 0 }, 150);
   });
 
-     $('#container').on('click','.item-remove',function(){
-        $(this).parent().remove();
-    });
-    
-    $.ajax('data/item.json', {
-    DataType: 'json',
+  $('#container').on('click','.item-remove',function(){
+    $(this).parent().remove();
+  });
+
+  $.ajax('data/item.json', {
+    dataType: 'json',
     contentType: 'application/json',
     cache: false
-    })
-        .done(function(respnse){
-        let items = respnse.items;
-        items.forEach(function(item){
-      addItem(item.name, item.description, item.price, item.moreInfo);
-        });
-    })
-        .fail(function(request, errorType, errorMessage){
-        console.log(errorMessage);
-        })
-        .always(function(){
-        
+  })
+  .done(function(response){
+    let items = response.items;
+    items.forEach(function(item){
+      addItem(item.id, item.name, item.description, item.price, item.moreInfo);
     });
-        
-        $('#container').on('click','.item-add',function(){
-            let id = $(this).parent().data('id');
-             $.ajax('data/addToCart.json', {
+  })
+  .fail(function(request, errorType, errorMessage){
+    console.log(errorMessage);
+  })
+  .always(function(){
+
+  });
+
+  $('#container').on('click','.item-add',function(){
+    let id = $(this).parent().data('id');
+
+    $.ajax('data/addToCart.json', {
       type: 'post',
       data: { id: id },
       dataType: 'json',
       contentType: 'application/json'
-        })
-            .done(function(response){
-        if (response.message === 'success'){
-            let price = response.price;
+    })
+    .done(function(response) {
+      if (response.message === 'success') {
+        let price = response.price;
 
-            cart += price;
-        
+        cart += price;
+
         $('#cart-container').text('$' + cart);
-        }
-                  
-         });
+      }
     });
+  });
+
+  $('#newsletter-checkbox').on('change',function(){
+    if ($(this).is(':checked')) {
+      $('#newsletter-frequency').fadeIn();
+    } else {
+      $('#newsletter-frequency').fadeOut();
+    }
+  });
+  $('#newsletter-checkbox').trigger('change');
+
+  $('#cart-form').on('submit',function(event){
+    event.preventDefault();
+
+    let data = { form: $(this).serialize(), price: cart };
+
+    $.ajax($(this).attr('action'), {
+      type: 'post',
+      data: data
+    })
+    .done(function(response){
+      $('#feedback-message').text(response.message);
+    });
+  });
 });
